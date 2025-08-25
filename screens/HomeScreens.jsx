6d78ff8df6b2ac82b52144e0a4854273743cf542
Ray20032000 <andrade.rayssa.da.silva@gmail.com>
2025-08-25 11:16:25 -0300

    create home screens

---
+++ screens/HomeScreens.jsx
@@ -0,0 +1,10 @@
+import {Text, View} from "react-native";
+import Header from "../components/Header"
+
+export default function HomeScreens() {
+    return (
+        <View>
+            <Header />
+        </View>
+    )
+}
